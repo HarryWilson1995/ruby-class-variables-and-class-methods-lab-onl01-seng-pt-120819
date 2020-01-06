2f61ac4a2b6a0204c9ebddc078e12429b149dc34
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-06 17:59:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,24 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0 
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @@song_count += 1 
+    @@genres << genre 
   end 
   
   def self.count 
     @@count 
+  end 
+  
+  def self.genres 
+    if @@genres.include?(genre)
+    @@genres 
+  end 
+  
+  def self.artists
+    @@artists
   end 
 end
